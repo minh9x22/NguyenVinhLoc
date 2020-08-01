001cdd9f179846ab7fbda473fdc45a91413fb606
lecongminh <lecongminh_99@yahoo.com>
2020-08-01 16:55:44 +0700

    first commit

---
+++ forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Email, EqualTo, InputRequired
+
+class SignUpForm(FlaskForm):
+    inputFirstName = StringField('First Name',
+        [DataRequired(message="Please enter your first name!")])
+    inputLastName = StringField('Last Name',
+        [DataRequired(message="Please enter your last name!")])
+    inputEmail = StringField('Email Address',
+        [Email(message="Not a valid email address!"),
+        DataRequired(message="Please enter your email address!")])
+    inputPassword = PasswordField('Password',
+        [InputRequired(message="Please enter your password!"),
+         EqualTo('inputConfirmPassword', message="Passwords does not match!")])
+    inputConfirmPassword = PasswordField('Confirm password')
+    submit = SubmitField('Sign Up')
+
+class SignInForm(FlaskForm):
+    inputEmail = StringField('Email Address',
+        [Email(message="Not a valid email address!"),
+        DataRequired(message="Please enter your email address!")])
+    inputPassword = PasswordField('Password',
+        [InputRequired(message="Please enter yout password!")])
+    submit = SubmitField('Sign In')
+
+class TaskForm(FlaskForm):
+    inputDescription = StringField('Task Description',
+        [DataRequired(message="Please enter your task content")])
+    inputPriority = SelectField('Priority', coerce = int)
+    submit = SubmitField('Create Task')
